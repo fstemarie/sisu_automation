f5104671cd808c4fb75488f3ca593463d1f6621c
fstemarie <14352349+fstemarie@users.noreply.github.com>
2026-06-15 12:25:03 -0400

    Ajoute outils

--- src/tools/delete_old_backups.fish
+++ src/tools/delete_old_backups.fish
@@ -1,10 +1,14 @@
-function delete_old_backups -a pattern nb_max \
+
+function delete_old_backups -a dst pattern nb_max \
     --description 'Supprime les anciennes sauvegardes en gardant au maximum $nb_max sauvegardes'
-    set -a files (eval "command ls -trd $pattern") # Récupère la liste des fichiers correspondant au pattern, triés par date de modification (du plus ancien au plus récent)
-    set nb_diff (math (count $files) - $nb_max)
-    if test $nb_diff -gt 0
-        set files $files[1..$nb_diff]
-        command rm -vf $files | tee -a $log
+    set files (string match -ie "$pattern" $dst/* | path sort -r) # Récupère la liste des fichiers correspondant au pattern, triés par ordre décroissant
+    if not test -d "$dst"
+        return 1
+    end
+    if test (count $files) -gt $nb_max
+        set low_index (math $nb_max + 1) # Calcule l'index du premier fichier à supprimer, qui est égal au nombre maximum de fichiers à garder + 2
+        set files $files[$low_index..-1]
+        command rm -vf -- $files
         return $pipestatus[1]
     else
         return 0 # Pas de fichiers à supprimer, tout va bien
